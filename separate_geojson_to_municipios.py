5ffe606e9b181b7fcc6f51bcd785beaab1768ce9
JoaoDias61 <jpdias641@gmail.com>
2023-01-11 15:30:18 -0300

    mover geojson to other page

---
+++ separate_geojson_to_municipios.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+
+estados_brasileiros = {
+    'AC': 'Acre',
+    'AL': 'Alagoas',
+    'AP': 'Amapá',
+    'AM': 'Amazonas',
+    'BA': 'Bahia',
+    'CE': 'Ceará',
+    'DF': 'Distrito Federal',
+    'ES': 'Espírito Santo',
+    'GO': 'Goiás',
+    'MA': 'Maranhão',
+    'MT': 'Mato Grosso',
+    'MS': 'Mato Grosso do Sul',
+    'MG': 'Minas Gerais',
+    'PA': 'Pará',
+    'PB': 'Paraíba',
+    'PR': 'Paraná',
+    'PE': 'Pernambuco',
+    'PI': 'Piauí',
+    'RJ': 'Rio de Janeiro',
+    'RN': 'Rio Grande do Norte',
+    'RS': 'Rio Grande do Sul',
+    'RO': 'Rondônia',
+    'RR': 'Roraima',
+    'SC': 'Santa Catarina',
+    'SP': 'São Paulo',
+    'SE': 'Sergipe',
+    'TO': 'Tocantins',
+}
+
+files = [file for file in os.listdir("./grid-por-cidade")]
+
+for file in files:
+    sigla_estado = file.split('.')[0]
+    if "AP" in file:
+        shutil.move(
+            f"./grid-por-cidade/{file}", f"./{estados_brasileiros[sigla_estado[-2:]]}")
